55db400ee849b44fca728850e88f9f0cf23453a8
Emre <aka548548@gmail.com>
2022-10-06 17:12:10 +0300

    Course Manager

---
+++ src/kodlama/io/business/course/CourseUpdateManager.java
@@ -0,0 +1,18 @@
+package kodlama.io.business.course;
+
+import kodlama.io.business.CommandService;
+import kodlama.io.dataAccess.CourseDao;
+import kodlama.io.entities.Course;
+
+public class CourseUpdateManager implements CommandService<Course> {
+    private CourseDao courseDao;
+
+    public CourseUpdateManager(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
+    @Override
+    public void Execute(Course course) {
+        this.courseDao.update(course);
+    }
+}
