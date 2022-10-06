6e18932a2bb4f7bbfd861cfc13f5e7aa6dfb91bd
Emre <aka548548@gmail.com>
2022-10-06 17:29:45 +0300

    Course Manager

--- src/kodlama/io/Main.java
+++ src/kodlama/io/Main.java
@@ -2,15 +2,16 @@ package kodlama.io;
 
 import kodlama.io.business.CommandService;
 import kodlama.io.business.course.CourseAddManager;
+import kodlama.io.core.logging.DatabaseLogger;
 import kodlama.io.core.logging.LoggingCommandServiceDecorator;
-import kodlama.io.dataAccess.HibernateCourseDao;
 import kodlama.io.dataAccess.JdbcCourseDao;
 import kodlama.io.entities.Course;
 
 public class Main {
     public static void main(String[] args) {
         CommandService<Course> courseAddCommandService =
-                new LoggingCommandServiceDecorator<Course>(new CourseAddManager(new JdbcCourseDao()));
+                new LoggingCommandServiceDecorator<Course>
+                        (new CourseAddManager(new JdbcCourseDao()), new DatabaseLogger());
 
         Course course = new Course();
         course.setName("Java");
--- src/kodlama/io/core/logging/LoggingCommandServiceDecorator.java
+++ src/kodlama/io/core/logging/LoggingCommandServiceDecorator.java
@@ -5,15 +5,22 @@ import kodlama.io.business.CommandService;
 public class LoggingCommandServiceDecorator<TCommand> implements CommandService<TCommand> {
 
     private final CommandService<TCommand> decoratee;
-    //inject logger and use it for logging!
+    private final Logger logger;
 
-    public LoggingCommandServiceDecorator(CommandService<TCommand> decoratee) {
+    public LoggingCommandServiceDecorator(CommandService<TCommand> decoratee, Logger logger) {
         this.decoratee = decoratee;
+        this.logger = logger;
     }
 
     @Override
     public void Execute(TCommand command) {
-        System.out.println("Logged");
+        this.logger.log("TEST");
         this.decoratee.Execute(command);
     }
+
+
+
+
+
+
 }
